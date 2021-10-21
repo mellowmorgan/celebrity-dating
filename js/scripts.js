b5a05f9bec3de841dba4717dcfcc762df7707686
Anna Pittman <annaepittman@gmail.com>
2021-10-21 11:34:51 -0700

    add else/if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-  $(#formCeleb).submit(function(event){
+  $("form#formCeleb").submit(function(event){
 
 
-//   const hobby = $("#hobby").val();
-//   const music = $("#music").val();
-//   const genderPref = $("#gender").val();
-//   let result;
+  const hobby = $("#hobby").val();
+  const music = $("#music").val();
+  const genderPref = $("#gender").val();
+  let result;
 
-// if (genderPref === "Female" && (music==="Country" || hobby==="Skydiving")) {
-//   result = "Nicole";
-// }
-// else if (genderPref==="Male" && (music==="Pop" || hobby==="Hiking")){
-//   result="Justin";
-// }
-// else if (genderPref==="Male" && (music==="Hip Hop/Rap" || hobby==="Playing guitar")){
-//   result="Timothee";
-// }
-// else if (genderPref ==="Either/or" && (music==="Hip Hop/Rap" || music==="Country" || music==="Playing guitar")) {
-//   result="Any";  
-// }
-// else {result="None"; }
+if (genderPref === "Female" && (music==="Country" || hobby==="Skydiving")) {
+  result = "Nicole";
+}
+else if (genderPref==="Male" && (music==="Pop" || hobby==="Hiking")){
+  result="Justin";
+}
+else if (genderPref==="Male" && (music==="Hip Hop/Rap" || hobby==="Playing guitar")){
+  result="Timothee";
+}
+else if (genderPref ==="Either/or" && (music==="Hip Hop/Rap" || music==="Country" || music==="Playing guitar")) {
+  result="Any";  
+}
+else {result="None"; }
 
-//$("#resultCeleb").text("test");
+$("#resultCeleb").text(result);
 $("#resultCeleb").show();
 event.preventDefault();
   });
